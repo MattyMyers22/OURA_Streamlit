e46d8c3bfd2da26b028f636824622b29d037d83d
MattyMyers22 <mmyers2206@gmail.com>
2023-08-08 22:34:38 -0700

    Added tab2 charts for average resting heart rate

--- oura_dashboard.py
+++ oura_dashboard.py
@@ -16,11 +16,21 @@ tab1, tab2, tab3, tab4, tab5 = st.tabs(['Heart Rate Variablity', 'Resting Heart 
 
 # Generate tab1 for HRV
 with tab1:
+    # Create two columns for charts
     col1, col2 = st.columns(2)
+    # Show past 90 hrv chart in col1
     col1.plotly_chart(oura_transform.hrv_past_90, use_container_width=True)
+    # Show quarterly hrv chart in col2
     col2.plotly_chart(oura_transform.hrv_qtr, use_container_width=True)
 
 # Generate tab2 for Resting Heart Rate
+with tab2:
+    # Create two column for holding charts
+    col1, col2 = st.columns(2)
+    # Show past 90-day Avg Resting Heart Rate chart
+    col1.plotly_chart(oura_transform.heart_rate_past_90, use_container_width=True)
+    # Show quarterly Avg Resting Heart Rate chart
+    col2.plotly_chart(oura_transform.heart_rate_qtr, use_container_width=True)
 
 # Generate tab3 for Respiratory Rate
 
