022d49ab4d3339c950c09e4107025ed52e40b142
MattyMyers22 <mmyers2206@gmail.com>
2023-08-09 22:10:38 -0700

    Finalized initial placements of charts in streamlit dashboard

--- oura_dashboard.py
+++ oura_dashboard.py
@@ -10,32 +10,30 @@ import streamlit as st
 # Streamlit dashboard title
 st.write('Matthew Myers Oura Ring Data')
 
-# Create sidebar option
-tab1, tab2, tab3, tab4, tab5 = st.tabs(['Heart Rate Variablity', 'Resting Heart Rate', 'Respiratory Rate', 'Sleep',
-                                        'Activity'])
+# Create two columns for charts
+col1, col2 = st.columns(2)
 
-# Generate tab1 for HRV
-with tab1:
-    # Create two columns for charts
-    col1, col2 = st.columns(2)
-    # Show past 90 hrv chart in col1
-    col1.plotly_chart(oura_transform.hrv_past_90, use_container_width=True)
-    # Show quarterly hrv chart in col2
-    col2.plotly_chart(oura_transform.hrv_qtr, use_container_width=True)
+# Show past 90 hrv chart in col1
+col1.plotly_chart(oura_transform.hrv_past_90, use_container_width=True)
+# Show quarterly hrv chart in col2
+col2.plotly_chart(oura_transform.hrv_qtr, use_container_width=True)
 
-# Generate tab2 for Resting Heart Rate
-with tab2:
-    # Create two column for holding charts
-    col1, col2 = st.columns(2)
-    # Show past 90-day Avg Resting Heart Rate chart
-    col1.plotly_chart(oura_transform.heart_rate_past_90, use_container_width=True)
-    # Show quarterly Avg Resting Heart Rate chart
-    col2.plotly_chart(oura_transform.heart_rate_qtr, use_container_width=True)
+# Show past 90-day Avg Resting Heart Rate chart
+col1.plotly_chart(oura_transform.heart_rate_past_90, use_container_width=True)
+# Show quarterly Avg Resting Heart Rate chart
+col2.plotly_chart(oura_transform.heart_rate_qtr, use_container_width=True)
 
-# Generate tab3 for Respiratory Rate
+# Show past 90-day Avg Respiratory Rate chart
+col1.plotly_chart(oura_transform.resp_rate_past_90, use_container_width=True)
+# Show quarterly Avg Respiratory Rate chart
+col2.plotly_chart(oura_transform.resp_rate_qtr, use_container_width=True)
 
-# Generate tab4 for Sleep
+# Show past 90-day Sleep Efficiency chart
+col1.plotly_chart(oura_transform.sleep_effic_past_90, use_container_width=True)
+# Show quarterly Sleep Efficiency chart
+col2.plotly_chart(oura_transform.sleep_effic_qtr, use_container_width=True)
 
-# Generate tab5 for Activity
-
-print('Run Ended')
+# Show past 90-day Steps chart
+col1.plotly_chart(oura_transform.steps_past_90, use_container_width=True)
+# Show quarterly Steps chart
+col2.plotly_chart(oura_transform.steps_qtr, use_container_width=True)
